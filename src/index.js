a641b43351af5f4893f996dfda5159b7724eabeb
Nico <nico@skubos.com>
2019-09-30 21:07:40 +0700

    seperates schema

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial on GraphQL'
+}]
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
-    info: () => `This is an API for HN`
+    info: () => `This is an API for HN`,
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`server is running on localhost:4000`))
